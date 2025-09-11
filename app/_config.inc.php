fef06aa7ed4612a3eed948f8984ff80fa7bc1f35
flanOcaramel <sael.sellesfrancesconi@outlook.com>
2025-09-11 13:54:35 +0200

    Initial commit

---
+++ app/_config.inc.php
@@ -0,0 +1,21 @@
+﻿<?php
+/**
+ * paramètres de configuration de l'application AgoraBo
+ * 
+ * @package default
+ * @author md
+ * @version    1.0
+ */
+
+// gestion d'erreur 
+ini_set('error_reporting', E_ALL);  	// en phase de développement
+//ini_set('error_reporting', 0);  		// en phase de production 
+
+// constantes pour l'accès à la base de données
+define('DB_SERVER', 'localhost');	// serveur MySql
+define('DB_DATABASE', 'jeux');		// nom de la base de données
+define('DB_USER', 'root');			// nom d'utilisateur
+define('DB_PWD', '');              	// mot de passe
+define('DSN','mysql:dbname='.DB_DATABASE.';host='.DB_SERVER);
+
+?>
